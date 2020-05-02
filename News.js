23ab50daac942eff45450cde220d31ceb71acf36
hazelcruzat09 <hazel.cruzat.09@gmail.com>
2020-05-02 14:24:08 +0800

    Search button effect for news fetch

--- News.js
+++ News.js
@@ -3,27 +3,33 @@ import React, {Component, useState, useEffect} from 'react';
 const News = () => {
   //state
 	const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('react')
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
 	//fetch data
 	const fetchNews = () => {
-	  fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+	  fetch(url)
 		.then(result => result.json())
 		.then(data => setNews(data.hits))
-		.catch(error => console.log(error))
+		.catch(error => console.log(error.message))
 	};
 	//effect
 	useEffect(() => {
-		fetchNews()
-	});
+		fetchNews();
+	}, [url]);
 
   const handleChange = e => {
     setSearchQuery(e.target.value);
-  }
+  };
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
 	//return jsx	
 	return(
 	<div>
 		<h2>News: </h2>
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <input type="text" value={searchQuery} onChange={handleChange} />
       <button>Search</button>
     </form>
