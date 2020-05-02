5dda1a522a299cf6953f71b52efbb762ee8445ba
hazelcruzat09 <hazel.cruzat.09@gmail.com>
2020-05-02 14:33:25 +0800

    With Loading

--- News.js
+++ News.js
@@ -2,20 +2,22 @@ import React, {Component, useState, useEffect} from 'react';
 
 const News = () => {
   //state
-	const [news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
   const [url, setUrl] = useState(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
-	//fetch data
-	const fetchNews = () => {
-	  fetch(url)
-		.then(result => result.json())
-		.then(data => setNews(data.hits))
-		.catch(error => console.log(error.message))
-	};
-	//effect
-	useEffect(() => {
-		fetchNews();
-	}, [url]);
+  const [loading, setLoading] = useState(false);
+  //fetch data
+  const fetchNews = () => {
+    setLoading(true)
+    fetch(url)
+    .then(result => result.json())
+    .then(data => (setNews(data.hits),setLoading(false)))
+    .catch(error => console.log(error.message))
+  };
+  //effect
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
 
   const handleChange = e => {
     setSearchQuery(e.target.value);
@@ -25,17 +27,18 @@ const News = () => {
     e.preventDefault();
     setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   };
-	//return jsx	
-	return(
-	<div>
-		<h2>News: </h2>
+  //return jsx  
+  return(
+  <div>
+    <h2>News: </h2>
+    {loading ? "Loading...." : ""}
     <form onSubmit={handleOnSubmit}>
       <input type="text" value={searchQuery} onChange={handleChange} />
       <button>Search</button>
     </form>
-		{news.map((n, i) => (<p key={i}>{n.title}</p>))}
-	</div>
-	);
+    {news.map((n, i) => (<p key={i}>{n.title}</p>))}
+  </div>
+  );
 };
 
 export default News;
