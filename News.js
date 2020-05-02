5dcbec12c8faf9442b9149495453af8da8fac060
hazelcruzat09 <hazel.cruzat.09@gmail.com>
2020-05-02 14:03:23 +0800

    news fetch with searchable field

--- News.js
+++ News.js
@@ -2,30 +2,34 @@ import React, {Component, useState, useEffect} from 'react';
 
 const News = () => {
   //state
-	const[news, setNews] = useState([]);
+	const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react')
 	//fetch data
 	const fetchNews = () => {
-		fetch(`http://hn.algolia.com/api/v1/search?query={react}`)
-		.then(response => response.json())
+	  fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+		.then(result => result.json())
 		.then(data => setNews(data.hits))
 		.catch(error => console.log(error))
 	};
 	//effect
 	useEffect(() => {
-		fetchNews();
+		fetchNews()
 	});
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  }
 	//return jsx	
 	return(
 	<div>
 		<h2>News: </h2>
     <form>
-      <input type="text" value ={} onChange={handleChange}>
-        <button>Search</button>
-      </input>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
     </form>
-		{news.map((n,i) => (<p key={i}>{n.title}</p>))}
+		{news.map((n, i) => (<p key={i}>{n.title}</p>))}
 	</div>
 	);
-}
+};
 
 export default News;
